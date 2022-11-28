5d72113ce75c5435ea22c20e1f0e3212817202c3
MatteoAndreoli <matteo.andreoli.business@gmail.com>
2022-11-29 00:49:50 +0100

    fixed roadmap

--- src/components/sections/Roadmap.js
+++ src/components/sections/Roadmap.js
@@ -20,9 +20,27 @@ margin-top:2rem;
 
 `
 const TitoloVignetta = styled.h2`
-font-size: ${props => props.theme.fontl};
-text-transform: capitalize;
-position:absolute;
+font-size: ${props => props.theme.fontxl};
+
+display:flex;
+margin: 1em;
+
+@media (max-width: 60em){
+
+  text-align:center;
+  font-size: ${props => props.theme.fontmd};
+}
+@media (max-width: 40em){
+
+  text-align:center;
+  font-size: ${props => props.theme.fontmd};
+}
+@media (max-width: 30em){
+
+  text-align:center;
+  font-size: 1em;
+  margin: 0.5em auto;
+}
 `
 const Section = styled.section`
 
@@ -72,6 +90,7 @@ flex-direction: column;
 
 
 
+
 `
 const Immagine1 = styled.img`
 max-width: 100%;
@@ -86,15 +105,55 @@ height:100%;
 width:100%;
 background-image: url(${Vignetta});
 
-
-height: 13vh;
+height: 22vh;
 width: 60vw;
-
-
+margin: 1em;
 align-items: center;
-justify-content: center;
 display: flex;
-margin-top:2rem;
+flex-direction: column;
+
+@media (max-width: 60em){
+  height: 30vh;
+  width: 80vw;
+  margin: 0.1em;
+}
+@media (max-width: 40em){
+  height: 30vh;
+  width: 80vw;
+  margin: 0.5em;
+}
+@media (max-width: 30em){
+  height: 30vh;
+  width: 80vw;
+  margin: 0.2em;
+}
+`
+const Descrizione = styled.p`
+font-size: ${props => props.theme.fonts};
+
+display:flex;
+margin-left: 2em;
+margin-right: 2em;
+text-align: center;
+
+@media (max-width: 60em){
+
+
+  font-size: 0.1em;
+}
+@media (max-width: 40em){
+
+
+  font-size: 0.1em;
+}
+@media (max-width: 30em){
+
+
+  font-size: 0.1em;
+  margin-top: 0.2 auto;
+  
+}
+
 `
 
 const Roadmap = () => {
@@ -109,19 +168,32 @@ const Roadmap = () => {
         <ContainerVignette>
 
             <Test>
-              WL 1st Fase
+              <TitoloVignetta>
+                WhiteList 1st Fase
+              </TitoloVignetta>
+              <Descrizione>Early stage stealth WhiteList [LINK]</Descrizione>
+
             </Test>
 
             <Test>
-              WL 2ndFase
+              <TitoloVignetta>
+                WhiteList 2ndFase
+              </TitoloVignetta>
+              <Descrizione>Early stage stealth WhiteList [LINK]</Descrizione>
             </Test>
 
             <Test>
-            MINT
+            <TitoloVignetta>
+              MINT
+            </TitoloVignetta>
+            <Descrizione>On June 10, each Dippie can claim their own Van which are the keys to the Dippieverse. Vans will expand the community and bring in other collections to integrate into our eco-system. Additionally, Vans give you access to the 'DippieRaces' P2E game.</Descrizione>
             </Test>
 
             <Test>
+            <TitoloVignetta>
               Mutant
+            </TitoloVignetta>
+            <Descrizione>Early stage stealth WhiteList edsiuhewudhewiuhdewiuheuhiuhriuhieu</Descrizione>
             </Test>
 
         </ContainerVignette>
