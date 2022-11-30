1995c7fb5509b00858cb4ecf2850085d4e8aa59f
MatteoAndreoli <matteo.andreoli.business@gmail.com>
2022-11-30 12:18:16 +0100

    fixed roadmap

--- src/components/sections/Roadmap.js
+++ src/components/sections/Roadmap.js
@@ -6,25 +6,15 @@ import Image from "../../assets/background/night.png"
 import Vignetta from "../../assets/elements/Vignetta.png"
 
 
-const UseVignetta = styled.div`
-height: 10vh;
-width: 50vw;
-background-color:red;
 
-
-align-items: center;
-justify-content: center;
-display: flex;
-margin-top: 1rem;
-
-
-`
 const TitoloVignetta = styled.h2`
 font-size: ${props => props.theme.fontxl};
 
+text-align: center;
 display:flex;
-margin-bottom: 0.5rem;
-margin-top: 0.5rem;
+margin-left: 2em;
+margin-right: 2em;
+margin-top: 0.5em;
 
 @media (max-width: 60em){
 
@@ -40,7 +30,6 @@ margin-top: 0.5rem;
 
   text-align:center;
   font-size: 1em;
-  margin: 0.5em auto;
 }
 `
 const Section = styled.section`
@@ -93,45 +82,34 @@ flex-direction: column;
 
 
 `
-const Immagine1 = styled.img`
-max-width: 100%;
-max-height: 100%;
-background-size:100% 100%;
-
-`
 const Test = styled.div`
 
 background-size:100% 100%;
-height:100%;
-width:100%;
 background-image: url(${Vignetta});
+display: inline-flex;
 
-height: 15vh;
-width: 40vw;
-
+max-width: 40vw;
+max-height: 40vw;
 margin: 1em;
 align-items: center;
-display: flex;
+
 flex-direction: column;
 
 @media (max-width: 60em){
-  height: 15vh;
-  width: 80vw;
+  max-width: 40vw;
   margin: 0.1em;
 }
 @media (max-width: 40em){
-  height: 16vh;
-  width: 80vw;
+  max-width: 40vw;
   margin: 0.5em;
 }
 @media (max-width: 30em){
-  height: 16vh;
-  width: 80vw;
+  max-width: 80vw;
+  max-height: 70vw;
   margin: 1em;
 }
 @media (max-width: 20em){
-  height: 22vh;
-  width: 80vw;
+  max-width: 80vw;
   margin: 0.8em;
 }
 `
@@ -139,28 +117,22 @@ const Descrizione = styled.p`
 font-size: ${props => props.theme.fontsm};
 
 display:flex;
-margin-left: 2em;
-margin-right: 2em;
+margin: 2em;
 
 text-align: center;
-margin-bottom: 1em;
+
 
 @media (max-width: 60em){
-
-
   font-size: 0.1em;
 }
+
 @media (max-width: 40em){
-
-
   font-size: 0.1em;
 }
+
 @media (max-width: 30em){
-
-
   font-size: 0.1em;
-  margin-top: 0.2 auto;
-  
+
 }
 
 `
@@ -180,31 +152,44 @@ const Roadmap = () => {
               <TitoloVignetta>
                 WhiteList 1st Fase
               </TitoloVignetta>
-              <Descrizione>Early stage stealth WhiteList [LINK]</Descrizione>
+              <Descrizione>Early stage stealth WhiteList edjne d d ed e er f er er e te t t t t e w tettwt tw tw tw tw tw PancakeSwap has the most users of any decentralized platform, ever.
+And those users are now entrusting the platform with over $6.1 billion in funds. PancakeSwap has the most users of any decentralized platform, ever.
+And those users are now entrusting the platform with over $6.1 billion in funds. PancakeSwap has the most users of any decentralized platform, ever.
+And those users are now entrusting the platform with over $6.1 billion in funds.[LINK]</Descrizione>
 
             </Test>
 
             <Test>
               <TitoloVignetta>
-                WhiteList 2ndFase
+                WhiteList 1st Fase
               </TitoloVignetta>
-              <Descrizione>Early stage stealth WhiteList [LINK]</Descrizione>
+              <Descrizione>Early stage</Descrizione>
+
             </Test>
 
             <Test>
-            <TitoloVignetta>
-              MINT
-            </TitoloVignetta>
-            <Descrizione>On June 10, each Dippie can claim their own Van which are the keys to the Dippieverse. Vans will expand the community and bring in other collections to integrate into our eco-system. Additionally, Vans give you access to the 'DippieRaces' P2E game.</Descrizione>
+              <TitoloVignetta>
+                WhiteList 1st Fase
+              </TitoloVignetta>
+              <Descrizione>Early stage</Descrizione>
+
             </Test>
 
             <Test>
-            <TitoloVignetta>
-              Mutant
-            </TitoloVignetta>
-            <Descrizione>Early stage stealth WhiteList edsiuhewudhewiuhdewiuheuhiuhriuhieu</Descrizione>
+              <TitoloVignetta>
+                WhiteList 1st Fase
+              </TitoloVignetta>
+              <Descrizione>Early stage</Descrizione>
+
             </Test>
 
+            <Test>
+              <TitoloVignetta>
+                WhiteList 1st Fase wewkhewiuhe
+              </TitoloVignetta>
+              <Descrizione>Early stag weiuwe ewiuw weiugwe uihweg weuigweb ewuighew ewuygw ewguie</Descrizione>
+
+            </Test>
         </ContainerVignette>
 
       </Container>
