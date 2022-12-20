bc0016da4f2ba328b6a98b307f658a0951756663
MatteoAndreoli <matteo.andreoli.business@gmail.com>
2022-12-20 11:33:47 +0100

    Fix Team Size

--- src/components/sections/Home.js
+++ src/components/sections/Home.js
@@ -8,12 +8,15 @@ min-height: ${props => `calc(100vh - ${props.theme.navHeight})`  };
 width: 100vw;
 position: relative;
 background-color: ${props => props.theme.body};
+align-items: center;
+justify-content: center;
+display: flex;
 `
 
 const Container = styled.div`
-width:75%;
+width: 75vw;
 min-height: 80vh;
-margin: 0 auto;
+
 background-color: ${props => props.theme.body};
 
 display: flex;
@@ -21,7 +24,7 @@ justify-content: center;
 align-items:center;
 
 @media (max-width: 64em){
-  width: 85%;
+
 }
 @media (max-width: 48em){
   flex-direction: column;
